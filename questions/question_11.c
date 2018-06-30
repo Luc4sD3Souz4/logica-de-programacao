123d7f0705d58fe6c1924e96920155ca6ac15045
Lucas De Souza <37718781+Luc4sD3Souz4@users.noreply.github.com>
2018-06-29 21:01:14 -0300

    Update question_11.c

--- questions/question_11.c
+++ questions/question_11.c
@@ -5,5 +5,20 @@
 //     Cada nota de bimestre é composta por 2 notas de provas.  
 
 main(){
+	float bim[3], nota[2];
+	float notafinal;
+	int i, j;
 	
+	for (i=1; i<4; i++) {
+		for (j=1; j<3; j++) {
+			printf("Informe a nota %d do %d bimestre : ", j, i);
+			scanf("%f", &nota[j]);
+			bim[i] = (nota[1] + nota[2]) / 2;
+		}
+	}
+	
+	notafinal = (bim[1] + bim[2] + bim[3]) / 3;
+	printf("Nota final = %.2f", notafinal);
+	
+	return 0;
 }
